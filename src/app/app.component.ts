94309ed093fd51f8e78b20d32a238ef6643422ef
LuisAvila2705 <luis.avila2705@gmail.com>
2024-02-19 13:35:25 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContadorComponent } from './contador/contador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'hello world';
 }
